770e1b9705dec4ad86188e57fc418518551d131d
Juan Bautista García Traver <juangarciatraver@gmail.com>
2024-10-22 10:57:39 +0200

    part1 courseinfo | Ejercicios 1.1-1.5

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,61 @@
+const Header = (header) => {
+  return (
+    <h1>{header.name}</h1>
+  )
+}
+
+const Part = (part) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = (content) => {
+  const parts = content.parts
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises}/>
+      <Part name={parts[1].name} exercises={parts[1].exercises}/>
+      <Part name={parts[2].name} exercises={parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const parts = props.parts
+  return (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App
