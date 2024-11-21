d3e801dc4b4faa7b4f70b8afe6f8749346f20fac
Juan Bautista García Traver <juangarciatraver@gmail.com>
2024-11-21 18:08:16 +0100

    part8 library - exercises 8.1-8.7

---
+++ part8/library/backend/.eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true
+    },
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'plugins': [
+        '@stylistic/js'
+    ],
+    'extends': 'eslint:recommended',
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'no-console': 0,
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 0,
+        'no-unused-vars': 0
+    }
+}
